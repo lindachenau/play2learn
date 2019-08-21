89bba2b9d078d3f6659b65b91ba894b386be9d37
Linda Chen <lindachenau@gmail.com>
2019-08-22 09:43:37 +1000

    baseline version

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <header>
             <div class="header-content">
                 <h1 class="title">Play To Learn Early Learning Centre</h1>
-                <h1 class="tagLine"><span class="dash">-</span>love . knowledge . respect . meaning . wisdom . intelligence</h1>
+                <h1 class="tagLine"><span class="dash">&ndash;</span>love . knowledge . respect . meaning . wisdom . intelligence</h1>
             </div>
         </header>
     </div>
